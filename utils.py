65df5b18d46dc64242ec9b6bc788c0fd25aa4e24
Chenyang Yuan <yuanchenyang@gmail.com>
2019-07-31 10:34:17 -0400

    Changed units for meteoblue

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from gbfs.client import GBFSClient
 
-
 class GBFSStationClient(GBFSClient):
     def __init__(self, url, language=None, json_fetcher=None):
         GBFSClient.__init__(self, url, language, json_fetcher)
@@ -17,7 +16,11 @@ class GBFSStationClient(GBFSClient):
 
 # Use this otherwise webpage returns browser unsupported error
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+COOKIES = {'precip': 'MILLIMETER',
+           'speed': 'KILOMETER_PER_HOUR',
+           'temp': 'CELSIUS'}
 
 def get_blooimage_src(url):
-    soup = BeautifulSoup(requests.get(url, headers=HEADERS).text)
-    return 'https:' + soup.find(id='blooimage').find('img')['data-original']
+    res = requests.get(url, headers=HEADERS, cookies=COOKIES)
+    soup = BeautifulSoup(res.text, features="html5lib")
+    return soup.find(id='blooimage').find('img')['data-original']
