af88def75893ae9c225b674bb21f7cbe61ee47ea
Chenyang Yuan <yuanchenyang@gmail.com>
2019-08-01 23:41:06 -0400

    Added bluebikes loading

--- dashboard.py
+++ dashboard.py
@@ -3,11 +3,10 @@ import argparse
 import requests
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from werkzeug.serving import BaseRequestHandler
 from utils import GBFSStationClient, get_blooimage_src
 
-BLUEBIKE_STATIONS = ['80', '184']
 BLUEBIKE_GBFS = 'https://gbfs.bluebikes.com/gbfs/gbfs.json'
 METEOBLUE_URL = 'https://www.meteoblue.com/en/weather/forecast/multimodel/cambridge_united-states-of-america_4931972'
 
@@ -25,17 +24,16 @@ def get_meteoblue():
         # TODO: add error image
         return '/static/img/favicon-32x32.png'
 
-
 @app.route('/get_bluebikes')
 def get_bluebikes():
     client = GBFSStationClient(BLUEBIKE_GBFS)
     stations = client.get_stations()
-    return json.dumps([stations[i] for i in BLUEBIKE_STATIONS])
+    requested = request.args.get('station_ids').split(',')
+    return json.dumps({i: stations[i] for i in requested})
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -7,14 +7,25 @@ function loadImages(){
     );
 }
 
+function loadMeteoblue(){
+    $.get('/get_meteoblue', function(data) {
+        $("#metoblue_img").attr("src", data);
+    });
+}
+
+function loadBluebikes(){
+    var stations = [80, 184, 67];
+    $.get('/get_bluebikes?station_ids=' + stations.join(','), function(data) {
+        data = JSON.parse(data);
+        stations.forEach(function (e) {
+            $("#bb-"+ e + "-bikes").text(data[e]['num_bikes_available']);
+            $("#bb-"+ e + "-docks").text(data[e]['num_docks_available']);
+        });
+    });
+}
 
 $(function() {
     loadImages();
-    $.ajax({
-        'url' : '/get_meteoblue',
-        'type' : 'GET',
-        'success' : function(data) {
-            $("#metoblue_img").attr("src", data);
-        }
-    });
+    loadMeteoblue();
+    loadBluebikes();
 });
