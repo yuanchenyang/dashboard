f3fddad0e14491488ee7e2385e96e7183143d934
Chenyang Yuan <yuanchenyang@gmail.com>
2022-01-07 22:25:13 -0500

    Added file-system caching

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,7 @@ import requests
 import json
 
 from flask import Flask, render_template, request
+from flask_caching import Cache
 from werkzeug.serving import WSGIRequestHandler
 from utils import GBFSStationClient, get_blooimage_src, scrape_wunderground,\
                   scrape_sailing_weather, get_next_bus_info, get_trash_info,\
@@ -11,13 +12,21 @@ from utils import GBFSStationClient, get_blooimage_src, scrape_wunderground,\
 
 BaseRequestHandler = WSGIRequestHandler
 
+config = dict(CACHE_TYPE="FileSystemCache",
+              CACHE_DEFAULT_TIMEOUT=300,
+              CACHE_THRESHOLD=10000,
+              CACHE_DIR="./cache")
+
 app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
 
 @app.route('/')
 def main_page():
     return render_template("index.html")
 
 @app.route('/get_meteoblue')
+@cache.cached(timeout=5*60)
 def get_meteoblue():
     try:
         return get_blooimage_src(request.args.get('url', ''))
@@ -26,6 +35,7 @@ def get_meteoblue():
         return '/static/img/favicon-32x32.png'
 
 @app.route('/get_bluebikes')
+@cache.cached(timeout=10)
 def get_bluebikes():
     client = GBFSStationClient()
     stations = client.get_stations()
@@ -33,22 +43,27 @@ def get_bluebikes():
     return json.dumps({i: stations[i] for i in requested})
 
 @app.route('/get_wunderground')
+@cache.cached(timeout=30)
 def get_wunderground():
     return scrape_wunderground(request.args.get('id'))
 
 @app.route('/get_sailing_weather')
+@cache.cached(timeout=30)
 def get_sailing_weather():
     return scrape_sailing_weather()
 
 @app.route('/get_nextbus')
+@cache.cached(timeout=30)
 def get_nextbus():
     return get_next_bus_info(request.args.get('stopid'))
 
 @app.route('/get_trash')
+@cache.cached(timeout=10*60)
 def get_trash():
     return get_trash_info(request.args.get('placeid'))
 
 @app.route('/get_bkb')
+@cache.cached(timeout=10*60)
 def get_bkb():
     return get_bkb_routesetting(request.args.get('cal_id'))
 
